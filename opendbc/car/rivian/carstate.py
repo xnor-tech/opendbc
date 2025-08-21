d56bb29246bb66cfe6cc7fcbb7b84e093d44bb95
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2025-08-21 21:02:52 +0200

    cruiseState enabled

--- opendbc/car/rivian/carstate.py
+++ opendbc/car/rivian/carstate.py
@@ -52,10 +52,10 @@ class CarState(CarStateBase, CarStateExt):
     if self.CP.carFingerprint == CAR.RIVIAN_R1_GEN1:
       speed = min(int(cp_adas.vl["ACM_tsrCmd"]["ACM_tsrSpdDisClsMain"]), 85)
       self.last_speed = speed if speed != 0 else self.last_speed
-      ret.cruiseState.enabled = cp_cam.vl["ACM_Status"]["ACM_FeatureStatus"] == 1
       # TODO: find cruise set speed on CAN
       ret.cruiseState.speed = self.last_speed * CV.MPH_TO_MS  # detected speed limit
 
+    ret.cruiseState.enabled = cp_cam.vl["ACM_Status"]["ACM_FeatureStatus"] == 1
     if not self.CP.openpilotLongitudinalControl:
       ret.cruiseState.speed = -1
     ret.cruiseState.available = True  # cp.vl["VDM_AdasSts"]["VDM_AdasInterfaceStatus"] == 1
