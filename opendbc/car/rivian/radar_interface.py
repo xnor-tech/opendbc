d346dc984c6a18305d204c0b5526e72aeb87e4bb
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2025-08-29 15:12:55 +0200

    update radar

--- opendbc/car/rivian/radar_interface.py
+++ opendbc/car/rivian/radar_interface.py
@@ -51,15 +51,17 @@ class RadarInterface(RadarInterfaceBase):
       msg = self.rcp.vl[f"RADAR_TRACK_{addr:x}"]
 
       # STATE 1=New 2=New_updated 3=Updated 4=Coasting 7=New_coasting
-      valid_state = msg['STATE'] in (1, 2, 3, 4, 7)
-      if valid_state:
+      valid = msg['STATE'] in (1, 2, 3, 4, 7)
+      if valid:
         if addr not in self.pts or msg['STATE'] in (1, 2, 7):
           self.pts[addr] = structs.RadarData.RadarPoint()
           self.pts[addr].trackId = self.track_id
           self.track_id += 1
+        else:
+          # MODE 1=SRR 2=LRR 3=SRR_and_LRR
+          valid = msg['MODE'] in (2, 3)
 
-        # MODE 1=SRR 2=LRR 3=SRR_and_LRR
-        self.pts[addr].measured = msg['MODE'] in (1, 2, 3) and msg['STATE'] in (2, 3)
+        self.pts[addr].measured = msg['STATE'] in (2, 3)
         azimuth = math.radians(msg['AZIMUTH'])
         self.pts[addr].dRel = math.cos(azimuth) * msg['LONG_DIST']
         self.pts[addr].yRel = 0.5 * -math.sin(azimuth) * msg['LONG_DIST']
@@ -67,7 +69,7 @@ class RadarInterface(RadarInterfaceBase):
         self.pts[addr].aRel = float('nan')
         self.pts[addr].yvRel = float('nan')
 
-      else:
+      if not valid:
         if addr in self.pts:
           del self.pts[addr]
 
