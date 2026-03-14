1eebccd33aba9a73b1a816224147395f5bb833d1
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2026-03-14 08:52:03 +0100

    add Tesla Model Y Juniper fingerprint

--- opendbc/car/tesla/fingerprints.py
+++ opendbc/car/tesla/fingerprints.py
@@ -41,6 +41,7 @@ FW_VERSIONS = {
       b'TeMYG4_Main_0.0.0 (77),Y4003.05.4',
       b'TeMYG4_Main_0.0.0 (67),Y4OC003.04.3',
       b'TeMYG4_Main_0.0.0 (77),Y4OC003.06.5',
+      b'TeMYG4_Main_0.0.0 (78),Y4OC003.07.0',
       b'\x01\x0113\x04\x00\x01\x00\x00\x00\t\x00\x00\x00\x01\x00\x00\xff\xff',
       b'\x01\x0113\x04\x00\x01\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\xff\xff',
     ],
